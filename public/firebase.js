d213fc9c15b9dbb009ec0b2ea2968ec7557ae100
Septimiu Draghici <septimiu.draghici@principal33.com>
2021-08-10 22:10:25 +0300

    Connected to firebase + insert done

---
+++ public/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCh5IQwqc7yjuwU4sOUGneANi6T7O4JE3A",
+    authDomain: "cms-angajati.firebaseapp.com",
+    projectId: "cms-angajati",
+    storageBucket: "cms-angajati.appspot.com",
+    messagingSenderId: "14029976152",
+    appId: "1:14029976152:web:127b37c10035dffaa6d02d",
+    measurementId: "G-WM0HZTB9MB"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+  const db = firebase.firestore();
+
+  export {db, firebase};
